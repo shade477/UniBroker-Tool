59184fb8e83eab82eff5048fb650460f7b666337
shade477 <sagardeyof477@gmail.com>
2024-10-26 16:50:07 +0530

    fail map

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import fetch
 def main():
     # print("Enter Choice: ")
     # key = input()
-    fetch.fetch('Zerodha')
+    fetch.fetch('Angel')
     # fetch.fetch_all()
 
 if __name__ == '__main__':
---
+++ unimap.py
@@ -0,0 +1,22 @@
+# Trading Symbol
+# Exchange
+# Instrument Type
+# ISIN (International Securities Identification Number)
+# Company Name/Description
+
+unimap = {
+    'Trading_Symbol': {
+        'fyers': 'underlying_symbol',
+        'angel': 'symbol',
+        'kotak': 'tradingsymbol',
+        'icici': 'exchangecode',
+        'zerodha': 'tradingsymbol'
+    },
+
+    'Exchange': {
+        'fyers': [0,3],
+        'angel': 'exch_seg',
+        'kotak': 'exchange',
+        'icici': 
+    }
+}
