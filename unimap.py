174b5a81838407f0dadd49d3e7c1b432c03073cd
shade477 <sagardeyof477@gmail.com>
2024-10-26 17:44:41 +0530

    fixed bug while concatenating dfs

--- unimap.py
+++ unimap.py
@@ -16,7 +16,8 @@ def download_master(broker):
     fetch.fetch(broker)
 
 def import_file(path):
-    df = pd.read_csv(path)
+    chunks = pd.read_csv(path, chunksize=10000, delimiter=',', on_bad_lines='skip')
+    df = pd.concat(chunks, ignore_index=True)  # Combine chunks into a single DataFrame
     return df
 
 def angel_map():
@@ -31,11 +32,33 @@ def zerodha_map():
     df = raw_df[['instrument_token', 'exchange_token', 'exchange', 'instrument_type', 'name']]
     return df
 
+def load_dirs(path, files):
+    dfs = []
+    for file in files:
+        raw_df = import_file(f'{path}/{file}.csv')
+        dfs.append(raw_df)
+    return dfs
 
+def kotak_map():
+    # download_master('Kotak')
+    path = 'datasets/Kotak'
+    raw_dfs = load_dirs(path, ['cash', 'futures'])
+    headers = ['instrumentToken','instrumentName','name',
+               'lastPrice','expiry','strike',
+               'tickSize','lotSize','instrumentType',
+               'segment','exchange','isin',
+               'multiplier','exchangeToken','OptionType'
+    ]
+    for raw_df in raw_dfs:
+        raw_df.columns = headers
+    
+    merged_df = pd.concat(raw_dfs, axis=0, ignore_index=True)
+    return merged_df
 
 
 def main():
-    angel_map()
+    print(kotak_map())
+    # kotak_map()
 
 if __name__ == '__main__':
     main()
